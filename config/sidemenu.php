114adaae1f5545f968d67d868a60007877a91f63
munmun45 <manaranamunmun@gmail.com>
2025-09-11 18:14:19 +0530

    refactor(sidemenu): simplify menu structure and update links
    - Remove unused menu items and consolidate navigation
    - Change .html links to .php for consistency
    - Rename "Orders" to "Restaurant Manager" to better reflect functionality

--- config/sidemenu.php
+++ config/sidemenu.php
@@ -1,12 +1,12 @@
- <div class="main-nav">
+<div class="main-nav">
                <!-- Sidebar Logo -->
                <div class="logo-box">
-                    <a href="index.html" class="logo-dark">
+                    <a href="index.php" class="logo-dark">
                          <img src="assets/images/logo-sm.png" class="logo-sm" alt="logo sm">
                          <img src="assets/images/logo-dark.png" class="logo-lg" alt="logo dark">
                     </a>
 
-                    <a href="index.html" class="logo-light">
+                    <a href="index.php" class="logo-light">
                          <img src="assets/images/logo-sm.png" class="logo-sm" alt="logo sm">
                          <img src="assets/images/logo-white.png" class="logo-lg" alt="logo light">
                     </a>
@@ -17,42 +17,23 @@
                     <ul class="navbar-nav" id="navbar-nav">
 
                          <li class="menu-item pt-2">
-                              <a class="menu-link" href="index.html">
+                              <a class="menu-link" href="index.php">
                                    <span class="nav-icon">
                                         <i class="ri-dashboard-2-line"></i>
                                    </span>
                                    <span class="nav-text"> Dashboard </span>
-                                   <span class="badge bg-success badge-pill text-end">9+</span>
                               </a>
                          </li>
 
                          <li class="menu-item">
-                              <a class="menu-link" href="orders.html">
+                              <a class="menu-link" href="restaurant-manager.php">
                                    <span class="nav-icon">
-                                        <i class="ri-shopping-cart-line"></i>
+                                        <i class="ri-restaurant-2-line"></i>
                                    </span>
-                                   <span class="nav-text"> Orders </span>
+                                   <span class="nav-text"> Restaurant Manager </span>
                               </a>
                          </li>
 
-                         <li class="menu-item">
-                              <a class="menu-link menu-arrow" href="#sidebarProduct" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarProduct">
-                                   <span class="nav-icon">
-                                        <i class="ri-shopping-basket-2-line"></i>
-                                   </span>
-                                   <span class="nav-text"> Products </span>
-                              </a>
-                              <div class="collapse" id="sidebarProduct">
-                                   <ul class="sub-menu-nav">
-                                        <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="product-listing.html">Listing</a>
-                                        </li>
-                                        <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="product-add-edit.html">Add/Edit Product</a>
-                                        </li>
-                                   </ul>
-                              </div>
-                         </li>
 
                          <li class="menu-item">
                               <a class="menu-link menu-arrow" href="#sidebarCategories" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarCategories">
@@ -73,199 +54,28 @@
                               </div>
                          </li>
 
-                         <li class="menu-item">
-                              <a class="menu-link" href="customers.html">
-                                   <span class="nav-icon">
-                                        <i class="ri-group-2-line"></i>
-                                   </span>
-                                   <span class="nav-text"> Customers </span>
-                              </a>
-                         </li>
+
 
                          <li class="menu-item">
-                              <a class="menu-link" href="menu-cards.html">
+                              <a class="menu-link menu-arrow" href="#sidebarProduct" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarProduct">
                                    <span class="nav-icon">
-                                        <i class="ri-restaurant-line"></i>
+                                        <i class="ri-shopping-basket-2-line"></i>
                                    </span>
-                                   <span class="nav-text"> Menu Cards </span>
+                                   <span class="nav-text"> Products </span>
                               </a>
-                         </li>
-
-                         <li class="menu-item">
-                              <a class="menu-link" href="pos.html">
-                                   <span class="nav-icon">
-                                        <i class="ri-mac-line"></i>
-                                   </span>
-                                   <span class="nav-text"> POS </span>
-                              </a>
-                         </li>
-
-                         <li class="menu-item">
-                              <a class="menu-link" href="reports.html">
-                                   <span class="nav-icon">
-                                        <i class="ri-bar-chart-box-ai-line"></i>
-                                   </span>
-                                   <span class="nav-text"> Reports </span>
-                              </a>
-                         </li>
-
-                         <li class="menu-item">
-                              <a class="menu-link" href="riders.html">
-                                   <span class="nav-icon">
-                                        <i class="ri-motorbike-line"></i>
-                                   </span>
-                                   <span class="nav-text"> Riders </span>
-                              </a>
-                         </li>
-
-                         <li class="menu-item">
-                              <a class="menu-link" href="offers.html">
-                                   <span class="nav-icon">
-                                        <i class="ri-discount-percent-line"></i>
-                                   </span>
-                                   <span class="nav-text"> Offers </span>
-                              </a>
-                         </li>
-
-                         <li class="menu-item">
-                              <a class="menu-link" href="locations.html">
-                                   <span class="nav-icon">
-                                        <i class="ri-map-pin-range-line"></i>
-                                   </span>
-                                   <span class="nav-text"> Locations </span>
-                              </a>
-                         </li>
-
-                         <li class="menu-item">
-                              <a class="menu-link" href="media.html">
-                                   <span class="nav-icon">
-                                        <i class="ri-image-circle-ai-line"></i>
-                                   </span>
-                                   <span class="nav-text"> Media </span>
-                              </a>
-                         </li>
-
-                         <li class="menu-item">
-                              <a class="menu-link" href="settings.html">
-                                   <span class="nav-icon">
-                                        <i class="ri-store-3-line"></i>
-                                   </span>
-                                   <span class="nav-text"> Store Settings </span>
-                              </a>
-                         </li>
-
-                         <li class="menu-item">
-                              <a class="menu-link" href="todo.html">
-                                   <span class="nav-icon">
-                                        <i class="ri-task-line"></i>
-                                   </span>
-                                   <span class="nav-text"> Todo </span>
-                              </a>
-                         </li>
-
-                         <li class="menu-item">
-                              <a class="menu-link" href="manage-apps.html">
-                                   <span class="nav-icon">
-                                        <i class="ri-apps-2-ai-line"></i>
-                                   </span>
-                                   <span class="nav-text"> Manage Apps </span>
-                              </a>
-                         </li>
-
-                         <li class="menu-item">
-                              <a class="menu-link menu-arrow" href="#sidebarInvoice" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarInvoice">
-                                   <span class="nav-icon">
-                                        <i class="ri-file-list-3-line"></i>
-                                   </span>
-                                   <span class="nav-text"> Invoices </span>
-                              </a>
-                              <div class="collapse" id="sidebarInvoice">
+                              <div class="collapse" id="sidebarProduct">
                                    <ul class="sub-menu-nav">
                                         <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="invoices.html">Invoices</a>
+                                             <a class="sub-menu-link" href="product-listing.html">Listing</a>
                                         </li>
                                         <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="invoice-details.html">Invoice Details</a>
+                                             <a class="sub-menu-link" href="product-add-edit.html">Add/Edit Product</a>
                                         </li>
                                    </ul>
                               </div>
                          </li>
 
-                         <li class="menu-item">
-                              <a class="menu-link menu-arrow" href="#sidebarPages" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarPages">
-                                   <span class="nav-icon">
-                                        <i class="ri-pages-line"></i>
-                                   </span>
-                                   <span class="nav-text"> Pages </span>
-                              </a>
-                              <div class="collapse" id="sidebarPages">
-                                   <ul class="sub-menu-nav">
-                                        <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="pages-starter.html">Welcome</a>
-                                        </li>
-                                        <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="pages-profile.html">Profile</a>
-                                        </li>
-                                        <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="pages-faqs.html">FAQs</a>
-                                        </li>
-                                        <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="pages-gallery.html">Gallery</a>
-                                        </li>
-                                        <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="pages-comingsoon.html">Coming Soon</a>
-                                        </li>
-                                        <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="pages-timeline.html">Timeline</a>
-                                        </li>
-                                        <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="pages-pricing.html">Pricing</a>
-                                        </li>
-                                        <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="pages-maintenance.html">Maintenance</a>
-                                        </li>
-                                        <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="pages-404.html">404 Error</a>
-                                        </li>
-                                   </ul>
-                              </div>
-                         </li>
-
-                         <li class="menu-item">
-                              <a class="menu-link menu-arrow" href="#sidebarAuthentication" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarAuthentication">
-                                   <span class="nav-icon">
-                                        <i class="ri-shield-user-line"></i>
-                                   </span>
-                                   <span class="nav-text"> Authentication </span>
-                              </a>
-                              <div class="collapse" id="sidebarAuthentication">
-                                   <ul class="sub-menu-nav">
-                                        <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="auth-signin.html">Sign In</a>
-                                        </li>
-                                        <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="auth-signup.html">Sign Up</a>
-                                        </li>
-                                        <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="auth-password.html">Reset Password</a>
-                                        </li>
-                                        <li class="sub-menu-item">
-                                             <a class="sub-menu-link" href="auth-lock-screen.html">Lock Screen</a>
-                                        </li>
-                                   </ul>
-                              </div>
-                         </li>
-
-                         <li class="menu-title">❋ UI Components ❋</li>
-
-                         <li class="menu-item">
-                              <a class="menu-link" href="ui-accordion.html">
-                                   <span class="nav-icon">
-                                        <i class="ri-shapes-line"></i>
-                                   </span>
-                                   <span class="nav-text"> Components </span>
-                              </a>
-                         </li>
+                         
                     </ul>
                </div>
           </div>
